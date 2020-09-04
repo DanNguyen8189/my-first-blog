5d6d0e8166a8fb13da57f2a6047d6b11fbb4a306
DanNguyen8189 <dannguyen8189@gmail.com>
2020-09-04 15:39:25 -0700

    Extended app to display individual posts

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,8 @@
 # Create your models here.
+# models are objects that get stored in the database
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
-
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #get the published posts (posts with publish date before the current time)
     return render(request, 'blog/post_list.html', {'posts': posts})
+    
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
