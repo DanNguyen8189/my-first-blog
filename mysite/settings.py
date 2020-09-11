bfc4ae43814872ad265cf8bae9bb9af88d0e79c8
DanNguyen8189 <dannguyen8189@gmail.com>
2020-09-11 11:59:06 -0700

    heroku changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,3 +123,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # for redirecting to the 
 LOGIN_REDIRECT_URL = '/' 
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
